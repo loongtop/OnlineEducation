55de41e417c13f7533481d8b17f01a601f45603e
leo <loongtop@gmail.com>
2022-07-16 11:00:58 -0400

    update SwaggerConfig.java

--- common/service_base/src/main/java/com/gkhy/servicebase/config/SwaggerConfig.java
+++ common/service_base/src/main/java/com/gkhy/servicebase/config/SwaggerConfig.java
@@ -3,28 +3,32 @@ package com.gkhy.servicebase.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableWebMvc
-//@Component
 @Configuration//Configuration class annotations
 @EnableSwagger2 //Swagger2 annotations
 public class SwaggerConfig {
     //http://localhost:8081/swagger-ui.html
+
     @Bean
     public Docket webApiConfig(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("webApi")
                 .apiInfo(webApiInfo())
                 .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.gkhy."))
                 .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
+//                .paths(Predicates.not(PathSelectors.any()))
                 .build();
 
     }
