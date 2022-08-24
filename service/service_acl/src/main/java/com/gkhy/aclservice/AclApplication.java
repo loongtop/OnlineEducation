6dbfe4016c7c87f094094f00b05794b0f77bc280
leo <loongtop@gmail.com>
2022-08-24 16:51:21 -0400

    Alpha 1.41 Error

--- service/service_acl/src/main/java/com/gkhy/aclservice/AclApplication.java
+++ service/service_acl/src/main/java/com/gkhy/aclservice/AclApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan(basePackages = "com.gkhy")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"com.gkhy"})
 public class AclApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(AclApplication.class);
